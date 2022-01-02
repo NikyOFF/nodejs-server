f4d16163398038f65b755a3c69a76ee882cc5621
NikyOFF <nikyoff@gmail.com>
2022-01-02 14:01:36 +0600

    Update ExpressApp.ts

--- src/classes/ExpressApp.ts
+++ src/classes/ExpressApp.ts
@@ -8,23 +8,13 @@ export default class ExpressApp {
     public server: http.Server = null;
     public expressApplication = express();
 
-    protected serverPort: number;
-    protected serverHostname: string;
-    protected databaseURI: string;
-
-    constructor() {
-        this.serverPort = config.SERVER_PORT;
-        this.serverHostname = config.SERVER_HOSTNAME;
-        this.databaseURI = config.DATABASE_URI;
-    }
-
     async start(): Promise<void> {
         console.clear();
 
         await loaders.init(this);
 
-        this.server = this.expressApplication.listen(this.serverPort, this.serverHostname, () => {
-            Logger.info(`Server started on ${config.SERVER_PROTOCOL}://${this.serverHostname}:${this.serverPort}/`, {label: "Express"});
+        this.server = this.expressApplication.listen(config.SERVER_PORT, config.SERVER_HOSTNAME, () => {
+            Logger.info(`Server started on ${config.SERVER_PROTOCOL}://${config.SERVER_HOSTNAME}:${config.SERVER_PORT}/`, {label: "Express"});
         });
 
         this.server.on('error', error => {
